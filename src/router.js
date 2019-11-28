5edcd86acfb052f40425f6c18c76b7741f5bdec3
muhammadawd <devmohamedawd@gmail.com>
2019-11-28 11:43:27 +0200

    recode

---
+++ src/router.js
@@ -0,0 +1,60 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import DashboardLayout from '@/layout/DashboardLayout'
+import AuthLayout from '@/layout/AuthLayout'
+
+Vue.use(Router);
+
+const router = new Router({
+    linkExactActiveClass: 'active',
+    routes: [
+        {
+            path: '/',
+            redirect: 'dashboard',
+            component: DashboardLayout,
+            children: [
+                {
+                    meta: {
+                        requiresAuth: true,
+                    },
+                    path: '/',
+                    name: 'dashboard',
+                    component: () => import(/* webpackChunkName: "demo" */ './views/Dashboard.vue')
+                }
+            ]
+        },
+        {
+            path: '/',
+            redirect: 'login',
+            component: AuthLayout,
+            children: [
+                {
+                    meta: {
+                        requiresAuth: false,
+                    },
+                    path: '/login',
+                    name: 'login',
+                    component: () => import(/* webpackChunkName: "demo" */ './views/Login.vue')
+                }
+            ]
+        }
+    ]
+});
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        let auth_data = localStorage.getItem('auth_data');
+        // this route requires auth, check if logged in
+        if (!auth_data) {
+            next({
+                name: 'login', // query: {redirect: to.fullPath}
+            })
+        } else {
+            next()
+        }
+    } else {
+        next() // make sure to always call next()!
+    }
+});
+
+export default router;
---
+++ src/views/Login.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="row justify-content-center">
+        <div class="col-lg-5 col-md-7">
+            <div class="card bg-secondary shadow border-0">
+                <div class="card-body px-lg-5 py-lg-5">
+                    <form role="form">
+                        <h1 class="text-center">Login</h1>
+                        <base-input class="input-group-alternative mb-3"
+                                    placeholder="Email"
+                                    addon-left-icon="ni ni-email-83"
+                                    v-model="model.email">
+                        </base-input>
+
+                        <base-input class="input-group-alternative"
+                                    placeholder="Password"
+                                    type="password"
+                                    addon-left-icon="ni ni-lock-circle-open"
+                                    v-model="model.password">
+                        </base-input>
+
+                        <div class="text-center">
+                            <base-button type="primary" class="my-4">Sign in</base-button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'login',
+        data() {
+            return {
+                model: {
+                    email: '',
+                    password: ''
+                }
+            }
+        }
+    }
+</script>
+<style>
+</style>
